ddfee2bc37e39b5b7a20615de31181bd138e4b79
David Ayala <davixyz@gmail.com>
2018-05-14 18:33:35 +0200

    header

--- static/js/custom.js
+++ static/js/custom.js
@@ -11,21 +11,6 @@ $(document).ready(function(){
 
 	$(".doingnow ul").addClass("default");
 
-	//FLICKR
-	/*$.getJSON("https://script.google.com/macros/s/AKfycbwSsffQhkYAK2xjRu1Ck0TYdg2uDnnTG7eJsfLXPUSEM2XhAyPM/exec?callback=?", function(data){
-		var stb = [];
-		var it;
-		var src;
-		for(var i=0;i<data.photoset.photo.length;i++){
-			it = data.photoset.photo[i];
-			src = "https://farm"+it.farm+".static.flickr.com/"+it.server+"/"+it.id+"_"+it.secret;
-			stb.push('<article><a href="',src,'_b.jpg" class="image featured"><img src="',src,'_n.jpg" alt="" /></a><!--header><h3><a href="#">',it.title,'</a></h3></header--><p>',it.title,'</p></article>');
-		}
-		if(stb.length>0){
-			$(stb.join("")).appendTo($(".flickr .reel"));
-		}
-	});*/
-
 	//TWEETS
 	$.getJSON("https://script.google.com/macros/s/AKfycbzLmZM7BRV_l4X9WL1h2QiQZmBrMZo19B6Eztx7ioT6osF9NZHg/exec?callback=?", function(data){
 		var stb = [], tweet_date, tcss="";
@@ -51,30 +36,9 @@ $(document).ready(function(){
 			return false;
 		});
 	});
-
-	//PROJECTS
-	/*var projects = [
-		{title : "markdown2json", link : "https://github.com/davidayalas/markdown2json", description : "easy markdown to json converter. Transform front matter properties and content into json key-values. Very easy to integrate with static sites generators. It creates an index that can be easily injected into algolia."},
-		{title : "Bluemix client", link : "https://github.com/davidayalas/bluemix-client", description : "Node.js Bluemix API client. It wraps CloudFoudry API and Containers API over a common interface."},
-		{title : "StaticDB", link : "https://github.com/davidayalas/staticdb", description : "generates hashed filenames with content from each row in a CSV that would we http-requested applying the same derive key algorithm. It provides the server process and the html client"},
-		{title : "go-scraper", link : "https://github.com/davidayalas/go-scraper", description : "proof of concept of Golang.org that scrapes content from a web application launching multiple and concurrent requests."},
-		{title : "gsa-jsonp-proxy", link : "https://github.com/davidayalas/gsa-jsonp-proxy", description : "a json proxy over google search protocol from <a href='https://support.google.com/gsa/'>Google Search Appliance</a>"},
-		{title : "gs-cache", link : "https://github.com/davidayalas/gscache", description : "Google Apps Sache and <k>scriptdb</k> SpreadSheet wrapper for Google Apps Script with real persistence and value splitting for large values (due to Google Apps Scripts limits)."},
-		{title : "gae-cache", link : "https://github.com/davidayalas/gae-cache", description : "cache and blobstore wrapper for Google App Engine (python) with real persistence and value splitting for large values (due to Google App Engine limits)."},
-		{title : "More mini projects here", link : "https://github.com/davidayalas/", description : ""},
-	];
-
-	var projects_html = [];
-
-	for(var i=0,z=projects.length;i<z;i++){
-		projects_html.push('<li><article class="post stub"><header><h3><a href="',projects[i].link,'">',projects[i].title,'</a></h3></header><p>',projects[i].description,'</p></article></li>');
-	}
-
-	$(projects_html.join("")).appendTo($("#projects ul"));
-	*/
 });
 
-$('body.homepage #header').css('background-image', 'url("../../images/header.jpg")');
+$('body.homepage #header').css('background-image', 'url("'+ (headerImage||null)+'")');
 
 /* google analytics basic setup */
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
