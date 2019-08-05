b8e489d2199304960e8a1e9b6507ba6115256f20
David Ayala Sánchez <david.ayala@uoc.edu>
2019-08-05 11:07:22 +0200

    new version

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: {
-      'postcss-uncss' : {
-        html: ['../public/*.html']
-      } 
-    }
-  }
---
+++ uncss.js
@@ -0,0 +1,14 @@
+var uncss = require('uncss');
+uncss
+var files   = ['index.html', 'gallery/index.html','gallery/london-2014/index.html'],
+    options = {
+        banner       : false,
+        stylesheets : ['assets/css/main.css','assets/css/font-awesome.min.css'],
+        ignore      : [".hidden",".tweet",".fa-plus-circle"],
+        output       : 'assets/css/styles.css'
+    };
+
+uncss(files, options, function (error, output) {
+    console.log(error);
+    console.log(output);
+});
