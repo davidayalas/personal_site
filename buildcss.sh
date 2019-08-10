2e89211b1fb2ab9aa7da560fc40eb691fad3a93f
David Ayala Sánchez <david.ayala@uoc.edu>
2019-08-10 10:18:36 +0200

    new version

--- buildcss.sh
+++ buildcss.sh
@@ -1,5 +1,5 @@
 hugo
-node _tasks/css/uncss.js > static/assets/css/styles.css    
+node _tasks/css/uncss.js > static/assets/css/style.css    
 hugo server &
 node _tasks/css/critical.js http://localhost:1313 $(pwd)/layouts/partials/critical-css-home.html
 node _tasks/css/critical.js http://localhost:1313/gallery/  $(pwd)/layouts/partials/critical-css-gallery.html
