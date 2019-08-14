d621a06d57e2bc2f8d13ac173e771a15d10401f4
David Ayala Sánchez <david.ayala@uoc.edu>
2019-08-14 18:48:46 +0200

    webp

--- _tasks/webp/index.js
+++ _tasks/webp/index.js
@@ -4,8 +4,11 @@ var _folder = "public/media/";
 
 fs.readdir(_folder, (err, files) => {
   files.forEach(file => {
+    if(file.slice(file.lastIndexOf("."))===".html"){
+        return;
+    }
     webp.cwebp(_folder+file,_folder+file+".webp","-q 100",function(status,error){
-        console.log(status,error);	
+        //console.log(status,error);	
       });
     });
 });
