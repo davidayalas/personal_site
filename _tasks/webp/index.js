af024d64b5eb16687beff0d92d3aa90e6a3febdb
David Ayala Sánchez <david.ayala@uoc.edu>
2019-08-14 14:16:21 +0200

    webp

--- _tasks/webp/index.js
+++ _tasks/webp/index.js
@@ -8,7 +8,7 @@ fs.readFile('../../public/index.html', 'utf8', function read(err, data) {
 
     data = data.slice(data.indexOf("headerImage")+15);
     data = data.slice(0,data.indexOf("';")).replace(/\\/g,"");
-    webp.cwebp("../../resources/_gen/images"+data,"../../resources/_gen/images"+data+".webp","-q 80",function(status,error){
+    webp.cwebp("../../public/"+data,"../../public/"+data+".webp","-q 80",function(status,error){
       	console.log(status,error);	
     });
 });
