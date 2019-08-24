741e9408bf2126dfa9768a9b26fa7d0ddef4bb3d
David Ayala Sánchez <david.ayala@uoc.edu>
2019-08-24 11:56:04 +0200

    pinned tweet image

--- _tasks/css/uncss.js
+++ _tasks/css/uncss.js
@@ -3,7 +3,7 @@ var uncss = require('uncss');
 var files   = [__dirname+'/../../public/index.html', __dirname+'/../../public/gallery/index.html',__dirname+'/../../public/gallery/london-2014/index.html'],
     options = {
         stylesheets  : ['assets/css/main.css',/*'assets/css/fontello.css','assets/css/sourcesanspro.css'*/],
-        ignore       : [".hidden",".tweet"/*,".fa-plus-circle"*/,".pull-right","ul.default"],
+        ignore       : [".hidden",".tweet","ul.tweets article img",".pull-right","ul.default"],
     };
 
 uncss(files, options, function (error, output) {
