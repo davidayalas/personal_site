6c7a68f28bd7fb2247795f3f41a7a1168f0594c8
davidayalas <davixyz@gmail.com>
2020-07-18 10:44:34 +0200

    trying solve problem with webp script on netlify

--- _tasks/webp/index.js
+++ _tasks/webp/index.js
@@ -2,6 +2,15 @@ var webp = require('webp-converter');
 var fs = require('fs');
 var _folder = "public/media/"; 
 
+/*fs
+  .readdirSync(_folder)
+  .filter(file => (file.slice(file.lastIndexOf("."))!==".html"))
+  .forEach((file) => {
+    webp.cwebp(_folder+file,_folder+file+".webp","-q 100",function(status,error){
+      //console.log(status,error);	
+    });
+  }*/
+
 fs.readdir(_folder, (err, files) => {
   files.forEach(file => {
     if(file.slice(file.lastIndexOf("."))===".html"){
