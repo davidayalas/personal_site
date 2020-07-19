3524ed6c79e6e1c9f8254a92f0909100f35ac7d7
davidayalas <davixyz@gmail.com>
2020-07-19 10:28:45 +0200

    webp conversion with binary cwebp

--- _tasks/webp/index.js
+++ _tasks/webp/index.js
@@ -1,23 +1,35 @@
-var webp = require('webp-converter');
-var fs = require('fs');
-var _folder = "public/media/"; 
+const fs = require('fs');
+const _path = "_tasks/webp"
+const _folder = __dirname.replace(_path,"") + "public/media/"; 
 
-/*fs
+
+function execShellCommand(cmd) {
+  const exec = require('child_process').exec;
+  return new Promise((resolve, reject) => {
+   exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+     console.warn(error);
+    }
+    resolve(stdout? stdout : stderr);
+   });
+  });
+ }
+
+fs
   .readdirSync(_folder)
-  .filter(file => (file.slice(file.lastIndexOf("."))!==".html"))
-  .forEach((file) => {
-    webp.cwebp(_folder+file,_folder+file+".webp","-q 100",function(status,error){
-      //console.log(status,error);	
-    });
-  }*/
+  .filter(file => (file.slice(file.lastIndexOf("."))!==".html" && file.slice(file.lastIndexOf("."))!==".webp"))
+  .forEach(async (file) => {
+    console.log(file+".webp");
+    await execShellCommand(_path+'/bin/cwebp ' + _folder+file + " -o " + _folder+file + ".webp");
+  })
 
-fs.readdir(_folder, (err, files) => {
+/*fs.readdir(_folder, (err, files) => {
   files.forEach(file => {
-    if(file.slice(file.lastIndexOf("."))===".html"){
+    if(file.slice(file.lastIndexOf("."))===".html" || file.slice(file.lastIndexOf("."))===".webp"){
         return;
     }
     webp.cwebp(_folder+file,_folder+file+".webp","-q 100",function(status,error){
-        //console.log(status,error);	
-      });
+      console.log(status,error);	
     });
-});
+  });
+});*/
