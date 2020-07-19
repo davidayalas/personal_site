1e872a6a3879cb52b97c279329b369d53a226053
davidayalas <davixyz@gmail.com>
2020-07-19 10:38:15 +0200

    webp again....

--- _tasks/webp/index.js
+++ _tasks/webp/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
+const webp = require('webp-converter');
 const _path = "_tasks/webp"
 const _folder = __dirname.replace(_path,"") + "public/media/"; 
 
-
-function execShellCommand(cmd) {
+/*function execShellCommand(cmd) {
   const exec = require('child_process').exec;
   return new Promise((resolve, reject) => {
    exec(cmd, (error, stdout, stderr) => {
@@ -21,15 +21,15 @@ fs
   .forEach(async (file) => {
     console.log(file+".webp");
     await execShellCommand(_path+'/bin/cwebp ' + _folder+file + " -o " + _folder+file + ".webp");
-  })
+  })*/
 
-/*fs.readdir(_folder, (err, files) => {
+fs.readdir(_folder, (err, files) => {
   files.forEach(file => {
     if(file.slice(file.lastIndexOf("."))===".html" || file.slice(file.lastIndexOf("."))===".webp"){
         return;
     }
     webp.cwebp(_folder+file,_folder+file+".webp","-q 100",function(status,error){
-      console.log(status,error);	
+      //console.log(status,error);	
     });
   });
-});*/
+});
