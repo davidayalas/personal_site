2e693e2bb7fef8ef08135c92b23899de827ef4ca
davidayalas <david.ayala@uoc.edu>
2026-04-01 11:35:09 +0200

    fix: update service worker fetch event handling and improve image assets

--- static/assets/js/sw.js
+++ static/assets/js/sw.js
@@ -1,7 +1,11 @@
+self.addEventListener('fetch', (event) => {
+  // passthrough (no cache aún)
+});
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  // passthrough (no cache aún)
+self.addEventListener('activate', (e) => {
+  clients.claim();
 });
